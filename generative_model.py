ebcd10ef534d40d530894f4307b887e98a6c4a27
HaCH <69476769+TinkrLouie@users.noreply.github.com>
2024-01-20 15:19:26 +0000

    update

--- generative_model.py
+++ generative_model.py
@@ -5,7 +5,6 @@ import torch
 from torch import nn
 import torchvision
 from cleanfid import fid
-import torchvision.utils as vutils
 from torchvision.utils import save_image
 import os
 from torchvision.datasets import CIFAR100
@@ -42,13 +41,7 @@ params = {
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-
-# helper function to make getting another batch of data easier
-#def cycle(iterable):
-#    while True:
-#        for x in iterable:
-#            yield x
+print(f"Using device: {device}\t" + (f"{torch.cuda.get_device_name(0)}" if torch.cuda.is_available() else "CPU"))
 
 
 class_names = ['apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle', 'bicycle', 'bottle', 'bowl',
@@ -69,51 +62,30 @@ transform = Compose([
     Lambda(lambda x: (x - 0.5) * 2)]
 )
 
-#train_loader = DataLoader(
-#    torchvision.datasets.CIFAR100('datasets', train=True, download=True, transform=torchvision.transforms.Compose([
-#        torchvision.transforms.ToTensor()
-#    ])),
-#    batch_size=64, drop_last=True)
-#
-#test_loader = DataLoader(
-#    torchvision.datasets.CIFAR100('datasets', train=False, download=True, transform=torchvision.transforms.Compose([
-#        torchvision.transforms.ToTensor()
-#    ])),
-#    batch_size=64, drop_last=True)
-
 ds_fn = CIFAR100
-dataset = ds_fn("./datasets", download=True, train=True, transform=transform)
-loader = DataLoader(dataset, params['batch_size'], shuffle=True)
+train_dataset = ds_fn("./datasets", download=True, train=True, transform=transform)
+loader = DataLoader(train_dataset, params['batch_size'], shuffle=True)
+
+test_dataset = ds_fn("./datasets", download=True, train=False, transform=transform)
+test_loader = DataLoader(test_dataset, params['batch_size'], shuffle=True)
+
+print(f'Size of training dataset: {len(loader.dataset)}')
+print(f'Size of testing dataset: {len(test_loader.dataset)}')
 
 
-# Getting device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using device: {device}\t" + (f"{torch.cuda.get_device_name(0)}" if torch.cuda.is_available() else "CPU"))
-
-
-print(f'Size of dataset: {len(loader.dataset)}')
-#train_iterator = iter(cycle(train_loader))
-#test_iterator = iter(cycle(test_loader))
-#
-#print(f'> Size of training dataset {len(train_loader.dataset)}')
-#print(f'> Size of test dataset {len(test_loader.dataset)}')
-
-
-# DDPM class
-class MyDDPM(nn.Module):
-    def __init__(self, network, n_steps=1000, device=None, image_chw=(params['n_channels'], params['dim'], params['dim'])):
-        super(MyDDPM, self).__init__()
+# Diffusion Model
+class DDPM(nn.Module):
+    def __init__(self, network, n_steps=1000, device=None):
+        super(DDPM, self).__init__()
         self.n_steps = n_steps
         self.device = device
-        self.image_chw = image_chw
         self.network = network.to(device)
         self.betas = torch.linspace(params['min_beta'], params['max_beta'], n_steps).to(
-            device)  # Number of steps is typically in the order of thousands
+            device)
         self.alphas = 1 - self.betas
         self.alpha_bars = torch.tensor([torch.prod(self.alphas[:i + 1]) for i in range(len(self.alphas))]).to(device)
 
     def forward(self, x0, t, eta=None):
-        # Make input image more noisy (we can directly skip to the desired step)
         n, c, h, w = x0.shape
         a_bar = self.alpha_bars[t]
         if eta is None:
@@ -123,9 +95,41 @@ class MyDDPM(nn.Module):
         return noisy
 
     def backward(self, x, t):
-        # Run each image through the network for each timestep t in the vector t.
-        # The network returns its estimation of the noise that was added.
         return self.network(x, t)
+
+    def sample_from_latent(self, latent, device=None):
+        with torch.no_grad():
+            if device is None:
+                device = self.device
+
+            # Copy latent to device
+            latent = latent.to(device)
+
+            # Start with the given latent
+            x = latent
+
+            for idx, t in enumerate(list(range(self.n_steps))[::-1]):
+                # Estimate noise to be removed
+                time_tensor = (torch.ones(latent.shape[0], 1) * t).to(device).long()
+                eta_theta = self.backward(x, time_tensor)
+
+                alpha_t = self.alphas[t]
+                alpha_t_bar = self.alpha_bars[t]
+
+                # Partially denoise the image
+                x = (1 / alpha_t.sqrt()) * (x - (1 - alpha_t) / (1 - alpha_t_bar).sqrt() * eta_theta)
+
+                if t > 0:
+                    z = torch.randn_like(latent)
+
+                    # Choose sigma_t calculation method (adjust as needed)
+                    beta_t = self.betas[t]
+                    sigma_t = beta_t.sqrt()  # Or use beta_tilda_t if preferred
+
+                    # Add controlled noise
+                    x = x + sigma_t * z
+
+        return x
 
 
 def sinusoidal_embedding(n, d):
@@ -140,9 +144,9 @@ def sinusoidal_embedding(n, d):
     return embedding
 
 
-class MyBlock(nn.Module):
+class Block(nn.Module):
     def __init__(self, shape, in_c, out_c, kernel_size=3, stride=1, padding=1, activation=None, normalize=True):
-        super(MyBlock, self).__init__()
+        super(Block, self).__init__()
         self.ln = nn.LayerNorm(shape)
         self.conv1 = nn.Conv2d(in_c, out_c, kernel_size, stride, padding)
         self.conv2 = nn.Conv2d(out_c, out_c, kernel_size, stride, padding)
@@ -158,9 +162,9 @@ class MyBlock(nn.Module):
         return out
 
 
-class MyUNet(nn.Module):
+class UNet(nn.Module):
     def __init__(self, n_steps=1000, time_emb_dim=100):
-        super(MyUNet, self).__init__()
+        super(UNet, self).__init__()
 
         # Sinusoidal embedding
         self.time_embed = nn.Embedding(n_steps, time_emb_dim)
@@ -170,25 +174,25 @@ class MyUNet(nn.Module):
         # First half
         self.te1 = self._make_te(time_emb_dim, 3)
         self.b1 = nn.Sequential(
-            MyBlock((params['n_channels'], params['dim'], params['dim']), 3, 10),
-            MyBlock((10, 32, 32), 10, 10),
-            MyBlock((10, 32, 32), 10, 10)
+            Block((3, 32, 32), 3, 10),
+            Block((10, 32, 32), 10, 10),
+            Block((10, 32, 32), 10, 10)
         )
         self.down1 = nn.Conv2d(10, 10, 4, 2, 1)
 
         self.te2 = self._make_te(time_emb_dim, 10)
         self.b2 = nn.Sequential(
-            MyBlock((10, 16, 16), 10, 20),
-            MyBlock((20, 16, 16), 20, 20),
-            MyBlock((20, 16, 16), 20, 20)
+            Block((10, 16, 16), 10, 20),
+            Block((20, 16, 16), 20, 20),
+            Block((20, 16, 16), 20, 20)
         )
         self.down2 = nn.Conv2d(20, 20, 4, 2, 1)
 
         self.te3 = self._make_te(time_emb_dim, 20)
         self.b3 = nn.Sequential(
-            MyBlock((20, 8, 8), 20, 40),
-            MyBlock((40, 8, 8), 40, 40),
-            MyBlock((40, 8, 8), 40, 40)
+            Block((20, 8, 8), 20, 40),
+            Block((40, 8, 8), 40, 40),
+            Block((40, 8, 8), 40, 40)
         )
         self.down3 = nn.Sequential(
             nn.Conv2d(40, 40, 2, 1),
@@ -199,9 +203,9 @@ class MyUNet(nn.Module):
         # Bottleneck
         self.te_mid = self._make_te(time_emb_dim, 40)
         self.b_mid = nn.Sequential(
-            MyBlock((40, 4, 4), 40, 20),
-            MyBlock((20, 4, 4), 20, 20),
-            MyBlock((20, 4, 4), 20, 40)
+            Block((40, 4, 4), 40, 20),
+            Block((20, 4, 4), 20, 20),
+            Block((20, 4, 4), 20, 40)
         )
 
         # Second half
@@ -213,25 +217,25 @@ class MyUNet(nn.Module):
 
         self.te4 = self._make_te(time_emb_dim, 80)
         self.b4 = nn.Sequential(
-            MyBlock((80, 8, 8), 80, 40),
-            MyBlock((40, 8, 8), 40, 20),
-            MyBlock((20, 8, 8), 20, 20)
+            Block((80, 8, 8), 80, 40),
+            Block((40, 8, 8), 40, 20),
+            Block((20, 8, 8), 20, 20)
         )
 
         self.up2 = nn.ConvTranspose2d(20, 20, 4, 2, 1)
         self.te5 = self._make_te(time_emb_dim, 40)
         self.b5 = nn.Sequential(
-            MyBlock((40, 16, 16), 40, 20),
-            MyBlock((20, 16, 16), 20, 10),
-            MyBlock((10, 16, 16), 10, 10)
+            Block((40, 16, 16), 40, 20),
+            Block((20, 16, 16), 20, 10),
+            Block((10, 16, 16), 10, 10)
         )
 
         self.up3 = nn.ConvTranspose2d(10, 10, 4, 2, 1)
         self.te_out = self._make_te(time_emb_dim, 20)
         self.b_out = nn.Sequential(
-            MyBlock((20, 32, 32), 20, 10),
-            MyBlock((10, 32, 32), 10, 10),
-            MyBlock((10, 32, 32), 10, 10, normalize=False)
+            Block((20, 32, 32), 20, 10),
+            Block((10, 32, 32), 10, 10),
+            Block((10, 32, 32), 10, 10, normalize=False)
         )
 
         self.conv_out = nn.Conv2d(10, 3, 3, 1, 1)
@@ -307,7 +311,45 @@ def training_loop(ddpm, loader, n_epochs, optim, device, store_path="ddpm_model.
         print(log_string)
 
 
-ddpm = MyDDPM(MyUNet(), device=device)
+def sample(ddpm, n_samples=10000, device=None, c=3, h=32, w=32):
+
+    with torch.no_grad():
+        if device is None:
+            device = ddpm.device
+
+        # Starting from random noise
+        x = torch.randn(n_samples, c, h, w).to(device)
+
+        for idx, t in enumerate(list(range(ddpm.n_steps))[::-1]):
+            # Estimating noise to be removed
+            time_tensor = (torch.ones(n_samples, 1) * t).to(device).long()
+            eta_theta = ddpm.backward(x, time_tensor)
+
+            alpha_t = ddpm.alphas[t]
+            alpha_t_bar = ddpm.alpha_bars[t]
+
+            # Partially denoising the image
+            x = (1 / alpha_t.sqrt()) * (x - (1 - alpha_t) / (1 - alpha_t_bar).sqrt() * eta_theta)
+
+            if t > 0:
+                z = torch.randn(n_samples, c, h, w).to(device)
+
+                # Option 1: sigma_t squared = beta_t
+                beta_t = ddpm.betas[t]
+                sigma_t = beta_t.sqrt()
+
+                # Option 2: sigma_t squared = beta_tilda_t
+                # prev_alpha_t_bar = ddpm.alpha_bars[t-1] if t > 0 else ddpm.alphas[0]
+                # beta_tilda_t = ((1 - prev_alpha_t_bar)/(1 - alpha_t_bar)) * beta_t
+                # sigma_t = beta_tilda_t.sqrt()
+
+                # Adding some more noise like in Langevin Dynamics fashion
+                x = x + sigma_t * z
+
+    return x
+
+
+ddpm = DDPM(UNet(), device=device)
 
 total_params = len(torch.nn.utils.parameters_to_vector(ddpm.parameters()))
 print(f'> Number of model parameters {total_params}')
@@ -321,67 +363,59 @@ if not params['no_train']:
 
 
 # Loading the trained model
-best_model = MyDDPM(MyUNet(), n_steps=1000, device=device)
+best_model = DDPM(UNet(), n_steps=1000, device=device)
 best_model.load_state_dict(torch.load(store_path, map_location=device))
 best_model.eval()
 print("Model loaded")
 
 # now show some interpolations (note you do not have to do linear interpolations as shown here, you can do non-linear or gradient-based interpolation if you wish)
-#col_size = int(np.sqrt(params['batch_size']))
-#
-#z0 = z[0:col_size].repeat(col_size, 1)  # z for top row
-#z1 = z[params['batch_size'] - col_size:].repeat(col_size, 1)  # z for bottom row
-#
-#t = torch.linspace(0, 1, col_size).unsqueeze(1).repeat(1, col_size).view(params['batch_size'], 1).to(device)
-#
-#lerp_z = (1 - t) * z0 + t * z1  # linearly interpolate between two points in the latent space
-#lerp_g = Gen.sample(lerp_z)  # sample the model at the resulting interpolated latents
-#
-#plt.rcParams['figure.dpi'] = 100
-#plt.grid(False)
-#plt.imshow(torchvision.utils.make_grid(lerp_g).cpu().numpy().transpose(1, 2, 0), cmap=plt.cm.binary)
-#plt.show()
-#
-## define directories
-#real_images_dir = 'real_images'
-#generated_images_dir = 'generated_images'
-#num_samples = 10000  # do not change
-#
-#
-## create/clean the directories
-#def setup_directory(directory):
-#    if os.path.exists(directory):
-#        shutil.rmtree(directory)  # remove any existing (old) data
-#    os.makedirs(directory)
-#
-#
-#setup_directory(real_images_dir)
-#setup_directory(generated_images_dir)
-#
-## generate and save 10k model samples
-#num_generated = 0
-#while num_generated < num_samples:
-#
-#    # sample from your model, you can modify this
-#    z = torch.randn(params['batch_size'], params['n_latent']).to(device)
-#    samples_batch = Gen.sample(z).cpu().detach()
-#
-#    for image in samples_batch:
-#        if num_generated >= num_samples:
-#            break
-#        save_image(image, os.path.join(generated_images_dir, f"gen_img_{num_generated}.png"))
-#        num_generated += 1
-#
-## save 10k images from the CIFAR-100 test dataset
-#num_saved_real = 0
-#while num_saved_real < num_samples:
-#    real_samples_batch, _ = next(test_iterator)
-#    for image in real_samples_batch:
-#        if num_saved_real >= num_samples:
-#            break
-#        save_image(image, os.path.join(real_images_dir, f"real_img_{num_saved_real}.png"))
-#        num_saved_real += 1
-#
-## compute FID
-#score = fid.compute_fid(real_images_dir, generated_images_dir, mode="clean")
-#print(f"FID score: {score}")
+z = sample(best_model, params['batch_size'], device=device)
+col_size = int(np.sqrt(params['batch_size']))
+
+z0 = z[0:col_size].repeat(col_size, 1)  # z for top row
+z1 = z[params['batch_size'] - col_size:].repeat(col_size, 1)  # z for bottom row
+
+t = torch.linspace(0, 1, col_size).unsqueeze(1).repeat(1, col_size).view(params['batch_size'], 1).to(device)
+
+lerp_z = (1 - t) * z0 + t * z1  # linearly interpolate between two points in the latent space
+lerp_g = best_model.sample(lerp_z)  # sample the model at the resulting interpolated latents
+
+plt.rcParams['figure.dpi'] = 100
+plt.grid(False)
+plt.imshow(torchvision.utils.make_grid(lerp_g).cpu().numpy().transpose(1, 2, 0), cmap=plt.cm.binary)
+plt.show()
+
+# define directories
+real_images_dir = 'real_images'
+generated_images_dir = 'generated_images'
+num_samples = 10000  # do not change
+
+
+# create/clean the directories
+def setup_directory(directory):
+    if os.path.exists(directory):
+        shutil.rmtree(directory)  # remove any existing (old) data
+    os.makedirs(directory)
+
+
+setup_directory(real_images_dir)
+setup_directory(generated_images_dir)
+
+# generate and save 10k model samples
+samples_batch = sample(best_model, num_samples, device=device)
+for i, image in enumerate(samples_batch):
+    save_image(image, os.path.join(generated_images_dir, f"gen_img_{i}.png"))
+
+# save 10k images from the CIFAR-100 test dataset
+num_saved_real = 0
+while num_saved_real < num_samples:
+    for _, batch in enumerate(loader):
+        for image in batch:
+            if num_saved_real >= num_samples:
+                break
+            save_image(image, os.path.join(real_images_dir, f"real_img_{num_saved_real}.png"))
+            num_saved_real += 1
+
+# compute FID
+score = fid.compute_fid(real_images_dir, generated_images_dir, mode="clean")
+print(f"FID score: {score}")
