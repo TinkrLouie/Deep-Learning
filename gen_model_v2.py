fb394d6c3ea7e974b0d6c9b82abccd2d4510b761
HaCH <69476769+TinkrLouie@users.noreply.github.com>
2024-01-21 01:47:51 +0000

    u

--- gen_model_v2.py
+++ gen_model_v2.py
@@ -133,24 +133,25 @@ class UNet(nn.Module):
         super().__init__()
         self.device = device
         self.time_dim = time_dim
-        self.inc = DoubleConv(c_in, 16)
-        self.down1 = Down(16, 32)
-        self.sa1 = SelfAttention(32, 32)
-        self.down2 = Down(32, 64)
-        self.sa2 = SelfAttention(64, 16)
-        self.down3 = Down(64, 64)
-        self.sa3 = SelfAttention(64, 8)
+        self.inc = DoubleConv(c_in, 64)
+        self.down1 = Down(64, 128)
+        self.sa1 = SelfAttention(128, 32)
+        self.down2 = Down(128, 256)
+        self.sa2 = SelfAttention(256, 16)
+        self.down3 = Down(256, 256)
+        self.sa3 = SelfAttention(256, 8)
 
-        self.bot1 = DoubleConv(64, 128)
-        self.bot2 = DoubleConv(128, 64)
+        self.bot1 = DoubleConv(256, 512)
+        self.bot2 = DoubleConv(512, 512)
+        self.bot3 = DoubleConv(512, 256)
 
-        self.up1 = Up(64, 64)
-        self.sa4 = SelfAttention(64, 8)
-        self.up2 = Up(64, 32)
-        self.sa5 = SelfAttention(32, 16)
-        self.up3 = Up(32, 16)
-        self.sa6 = SelfAttention(16, 16)
-        self.outc = nn.Conv2d(16, c_out, kernel_size=1)
+        self.up1 = Up(512, 128)
+        self.sa4 = SelfAttention(128, 16)
+        self.up2 = Up(256, 64)
+        self.sa5 = SelfAttention(64, 32)
+        self.up3 = Up(128, 64)
+        self.sa6 = SelfAttention(64, 64)
+        self.outc = nn.Conv2d(64, c_out, kernel_size=1)
 
     def pos_encoding(self, t, channels):
         inv_freq = 1.0 / (
@@ -176,6 +177,7 @@ class UNet(nn.Module):
 
         x4 = self.bot1(x4)
         x4 = self.bot2(x4)
+        x4 = self.bot3(x4)
 
         x = self.up1(x4, x3, t)
         x = self.sa4(x)
