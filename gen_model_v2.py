7907f2294b874c3e3170bcea37ade34754dd53cb
HaCH <69476769+TinkrLouie@users.noreply.github.com>
2024-01-22 23:13:45 +0000

    u

--- gen_model_v2.py
+++ gen_model_v2.py
@@ -146,11 +146,11 @@ class UNet(nn.Module):
 
         self.up1 = Up(128, 32)
         self.sa4 = SelfAttention(32, 8)
-        self.up2 = Up(64, 16)
-        self.sa5 = SelfAttention(16, 16)
-        self.up3 = Up(32, 16)
-        self.sa6 = SelfAttention(16, 32)
-        self.outc = nn.Conv2d(16, c_out, kernel_size=1)
+        self.up2 = Up(64, 32)
+        self.sa5 = SelfAttention(32, 16)
+        self.up3 = Up(64, 32)
+        self.sa6 = SelfAttention(32, 32)
+        self.outc = nn.Conv2d(32, c_out, kernel_size=1)
 
     def pos_encoding(self, t, channels):
         inv_freq = 1.0 / (
