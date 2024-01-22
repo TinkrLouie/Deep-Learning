ecf6508dfc9f8269ccddb04f0a9041f92c8b3896
HaCH <69476769+TinkrLouie@users.noreply.github.com>
2024-01-22 22:45:23 +0000

    u

--- gen_model_v2.py
+++ gen_model_v2.py
@@ -177,7 +177,9 @@ class UNet(nn.Module):
         x4 = self.bot3(x4)
 
         x = self.up1(x4, x3, t)
+        print(x.shape)
         x = self.sa4(x)
+        print(x.shape)
         x = self.up2(x, x2, t)
         x = self.sa5(x)
         x = self.up3(x, x1, t)
