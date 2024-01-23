1893658c472c9961f1e9d7361ab445539571e0f7
HaCH <69476769+TinkrLouie@users.noreply.github.com>
2024-01-23 23:50:54 +0000

    u

--- DCGAN.py
+++ DCGAN.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
+from matplotlib import pyplot as plt
 from torchvision.utils import save_image
 import numpy as np
 import random
@@ -13,7 +14,8 @@ from torch.utils.data import DataLoader
 from torchvision.transforms import Compose, ToTensor
 from torch.optim import Adam
 import torchvision.utils as vutils
-
+import matplotlib.animation as animation
+from IPython.display import HTML
 
 store_path = "vae_CIFAR100.pt"
 
@@ -232,3 +234,35 @@ if __name__ == '__main__':
 
             iters += 1
 
+    plt.figure(figsize=(10, 5))
+    plt.title("Generator and Discriminator Loss During Training")
+    plt.plot(G_losses, label="G")
+    plt.plot(D_losses, label="D")
+    plt.xlabel("iterations")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.show()
+
+    fig = plt.figure(figsize=(8, 8))
+    plt.axis("off")
+    ims = [[plt.imshow(np.transpose(i, (1, 2, 0)), animated=True)] for i in img_list]
+    ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
+
+    HTML(ani.to_jshtml())
+
+    real_batch = next(iter(test_loader))
+
+    # Plot the real images
+    plt.figure(figsize=(15, 15))
+    plt.subplot(1, 2, 1)
+    plt.axis("off")
+    plt.title("Real Images")
+    plt.imshow(
+        np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(), (1, 2, 0)))
+
+    # Plot the fake images from the last epoch
+    plt.subplot(1, 2, 2)
+    plt.axis("off")
+    plt.title("Fake Images")
+    plt.imshow(np.transpose(img_list[-1], (1, 2, 0)))
+    plt.show()
