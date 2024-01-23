717a2883bbde2e1f9e6bd9ea84ef08c7484da702
HaCH <69476769+TinkrLouie@users.noreply.github.com>
2024-01-23 00:54:31 +0000

    u

--- gen_model_v2.py
+++ gen_model_v2.py
@@ -146,9 +146,9 @@ class UNet(nn.Module):
 
         self.up1 = Up(64, 32)  # 8
         self.sa4 = SelfAttention(32)
-        self.up2 = Up(64, 16)  # 16
-        self.sa5 = SelfAttention(16)
-        self.up3 = Up(32, 16)  # 32
+        self.up2 = Up(64, 32)  # 16
+        self.sa5 = SelfAttention(32)
+        self.up3 = Up(64, 16)  # 32
         self.sa6 = SelfAttention(16)
         self.outc = nn.Conv2d(16, c_out, kernel_size=1)
 
