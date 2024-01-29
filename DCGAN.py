5cc1c24c719116cdebcc6c77e16c8323c3b84b12
HaCH <69476769+TinkrLouie@users.noreply.github.com>
2024-01-29 02:46:05 +0000

    u

--- DCGAN.py
+++ DCGAN.py
@@ -29,7 +29,7 @@ torch.manual_seed(SEED)
 params = {
     'batch_size': 64,
     'nc': 3,
-    'lr': 0.0001,  # 0.0002 => FID 81.57
+    'lr': 0.0005,  # 0.0002 => FID 81.57
     'step': 50000,
     'nz': 100,  # Size of z latent vector
     'real_label': 0.9,  # Label smoothing
